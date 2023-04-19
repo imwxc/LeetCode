56f546d842819fec8a03d67bc368b6ab35c2921a
imwxc <1362451456@qq.com>
2023-04-19 11:47:09 +0800

    update

--- src/JavaScript/1.两数之和.js
+++ src/JavaScript/1.两数之和.js
@@ -2,15 +2,13 @@
  * @lc app=leetcode.cn id=1 lang=javascript
  *
  * [1] 两数之和
+ * 思路：
+ * V1: 遍历数组，对每个x查找 target-x是否存在，时间 O(n^2)
+ * V2：利用Map，遍历每个x时查找 对象中是否有 target-x，有则返回，没有则将x存入Map中
  */
 
 // @lc code=start
-/**
- * @param {number[]} nums
- * @param {number} target
- * @return {number[]}
- */
-var twoSum = function(nums, target) {
+function V1(nums,target){
     var fidx = -1
     var secIdx = -1
     var res = []
@@ -22,6 +20,30 @@ var twoSum = function(nums, target) {
         fidx = idx
         secIdx = nums.findIndex((j,jdx)=>(j===(target - i) && idx !== jdx))
     })
+    return res
+}
+function V2(nums=[],target){
+    var map = new Map()
+    for(let i = 0;i<nums.length;i++){
+        const currNum = nums[i];
+        const targetNum = target - currNum;
+        let targetNumIdx = -1;
+        if(map.has(targetNum)){
+            targetNumIdx = map.get(targetNum)
+            return [targetNumIdx,i]
+        }else{
+            map.set(currNum,i)
+        }
+    }
+}
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    // res = V1(nums,target)
+    res = V2(nums,target)
     return res
 };
 // @lc code=end
